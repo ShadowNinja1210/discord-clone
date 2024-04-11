99b77a4ed7248a7b3c9f720233b03671a1ca4136
Mohit Jeswani <mohitjeswani1210@gmail.com>
2024-04-11 20:58:47 +0530

    11th April, 2024 - The application is working fine on Local host.

--- app/api/members/[memberId]/route.ts
+++ app/api/members/[memberId]/route.ts
@@ -1,26 +1,61 @@
-import { AccessToken } from "livekit-server-sdk";
-import { NextRequest, NextResponse } from "next/server";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
-  const room = req.nextUrl.searchParams.get("room");
-  const username = req.nextUrl.searchParams.get("username");
-  if (!room) {
-    return NextResponse.json({ error: 'Missing "room" query parameter' }, { status: 400 });
-  } else if (!username) {
-    return NextResponse.json({ error: 'Missing "username" query parameter' }, { status: 400 });
+export async function PATCH(req: Request, { params }: { params: { memberId: string } }) {
+  try {
+    const profile = await currentProfile;
+    const { searchParams } = new URL(req.url);
+    const { role } = await req.json();
+
+    const serverId = searchParams.get("serverId");
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!serverId) {
+      return new NextResponse("Server ID Missing", { status: 400 });
+    }
+
+    if (!params.memberId) {
+      return new NextResponse("Member ID Missing", { status: 400 });
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        profileId: profile.id,
+      },
+      data: {
+        members: {
+          update: {
+            where: {
+              id: params.memberId,
+              profileId: {
+                not: profile.id,
+              },
+            },
+            data: {
+              role,
+            },
+          },
+        },
+      },
+      include: {
+        members: {
+          include: {
+            profile: true,
+          },
+          orderBy: {
+            role: "asc",
+          },
+        },
+      },
+    });
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[MEMBER_ID_PATCH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
   }
-
-  const apiKey = process.env.LIVEKIT_API_KEY;
-  const apiSecret = process.env.LIVEKIT_API_SECRET;
-  const wsUrl = process.env.NEXT_PUBLIC_LIVEKIT_URL;
-
-  if (!apiKey || !apiSecret || !wsUrl) {
-    return NextResponse.json({ error: "Server misconfigured" }, { status: 500 });
-  }
-
-  const at = new AccessToken(apiKey, apiSecret, { identity: username });
-
-  at.addGrant({ room, roomJoin: true, canPublish: true, canSubscribe: true });
-
-  return NextResponse.json({ token: at.toJwt() });
 }
